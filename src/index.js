8c10a9990183908cfb377e0736cfdd62c069937c
Isac Vieira <isacvieira2009@hotmail.com>
2023-09-15 17:35:52 -0300

    tratando erros

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const routes = require("./routes");
+require("express-async-errors");
 
 const express = require("express");
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.sendStatus(500);
+});
 app.listen(3000, () =>
     console.log("Servidor rodando em http://localhost:3000")
 );
